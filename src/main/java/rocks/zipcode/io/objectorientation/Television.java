05b5c0da6313fe1c3648fba09c3d46910fde0b3f
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-04-16 10:54:15 -0400

    completed OOP all tests passed

--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,13 +1,32 @@
 package rocks.zipcode.io.objectorientation;
 
 public class Television {
-    public void turnOn() {
+
+    private boolean isTurnedOn;
+    private TVChannel tvChannel;
+
+    public Television() {
+        this.isTurnedOn = false;
+        this.tvChannel = TVChannel.getByOrdinal(0);
     }
 
-    public void setChannel(Integer channel) {
+    public Television(boolean isTurnedOn, TVChannel channel) {
+        this.isTurnedOn = isTurnedOn;
+        this.tvChannel = channel;
+    }
+
+    public void turnOn() {
+        isTurnedOn = !isTurnedOn;
+    }
+
+    public void setChannel(Integer channel) throws IllegalStateException{
+
+        if(isTurnedOn) {
+            tvChannel = TVChannel.getByOrdinal(channel);
+        } else {throw new IllegalStateException();}
     }
 
     public TVChannel getChannel() {
-        return null;
+        return tvChannel;
     }
 }
